d278e53064f3fc06b216ab2abfc371c0be0d38f4
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-09-28 14:22:27 -0700

    Added utilities to resolve 'latest' version aliases.

---
+++ R/resolveLatest.R
@@ -0,0 +1,39 @@
+#' Resolve the latest version
+#'
+#' Figure out the latest version of a project or file.
+#' 
+#' @param project String containing the project identifier.
+#' @param id String containing a file identifier.
+#' @param url String containing the URL of the ArtifactDB endpoint.
+#'
+#' @return 
+#' For \code{resolveLatestVersion}, a string containing the latest version in its canonical form.
+#'
+#' For \code{resolveLatestID}, \code{id} is updated with any \code{"latest"} version alias replaced by its canonical form.
+#'
+#' @author Aaron Lun
+#' @examples
+#' resolveLatestVersion(example.project, example.url)
+#'
+#' unpacked <- unpackID(example.id)
+#' unpacked$version <- "latest"
+#' latest.id <- do.call(packID, unpacked)
+#' resolveLatestID(latest.id, example.url)
+#' 
+#' @export
+#' @importFrom httr content
+resolveLatestVersion <- function(project, url) {
+    endpoint <- paste(url, "projects", project, "versions", sep="/")
+    output <- authorizedVerb(GET, url=endpoint)
+    checkResponse(output)
+    content(output)[["latest"]][["_extra.version"]]
+}
+
+#' @export
+resolveLatestID <- function(id, url) {
+    unpacked <- unpackID(id)
+    if (unpacked$version == "latest") {
+        unpacked$version <- resolveLatestVersion(unpacked$project, url)
+    }
+    do.call(packID, unpacked)
+}
