37f21dee6cec3e9b8715ef82cf78a1db83f1baee
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-08-08 11:14:44 -0700

    Added a simpler caching function based on file paths.
    This avoids performance issues and SQLite corruption with BiocFileCache.

--- R/caching.R
+++ R/caching.R
@@ -1,14 +1,22 @@
-#' Use the Bioconductor caching function
+#' Caching functions 
 #'
-#' This implements one possible \code{cache} function for use in \code{\link{getFileMetadata}} and friends.
-#' Caching is based on \pkg{BiocFileCache} with thread-safety enforced by \pkg{filelock}.
+#' This implements a few possible \code{cache} functions for use in \code{\link{getFileMetadata}} and friends.
 #'
-#' @param cache A BiocFileCache object.
+#' @param cache For \code{biocCache}, a BiocFileCache object.
+#' 
+#' For \code{simpleCache}, a string containing a path to a cache directory.
 #' @param key,save See \code{\link{getFileMetadata}} for details on the caching function.
 #' @param update Logical scalar indicating whether assets in the cache should be forcibly updated.
 #' Useful for resolving corrupted caches or incomplete downloads.
 #'
 #' @return String containing a path to the cached resource inside \code{cache}.
+#'
+#' @details
+#' For \code{simpleCache}, caching is done by downloading directly to the file system with no further tracking.
+#'
+#' For \code{biocCache}, caching is based on \pkg{BiocFileCache}, which tracks all calls via a central SQLite database.
+#'
+#' In all cases, downloads and access are protected by \pkg{filelock}, so multiple processes can query/update the cache safely.
 #'
 #' @author Aaron Lun
 #'
@@ -27,7 +35,37 @@
 #' @seealso
 #' \code{\link{getFileMetadata}} for more details on the caching function.
 #' 
+#' @name caching
+NULL
+
 #' @export
+#' @rdname caching
+#' @importFrom utils URLencode
+simpleCache <- function(cache, key, save, update=FALSE) {
+    path <- file.path(cache, URLencode(key, reserved=TRUE, repeated=TRUE))
+
+    # Acquiring a path lock so that processes don't use the downloaded file
+    # after it started but before it's finished.
+    path.lock <- paste0(path, "_zircon-LOCK")
+    plock <- filelock::lock(path.lock)
+    on.exit(filelock::unlock(plock), add=TRUE)
+
+    if (!file.exists(path) || update) {
+        success <- FALSE
+        on.exit({
+            if (!success) {
+                unlink(path, force=TRUE)
+            }
+        }, add=TRUE)
+        save(path)
+        success <- TRUE
+    }
+
+    path
+}
+
+#' @export
+#' @rdname caching
 biocCache <- function(cache, key, save, update=FALSE) {
     lockfile <- file.path(BiocFileCache::bfccache(cache), "zircon-LOCK")
     lck <- filelock::lock(lockfile)
@@ -73,5 +111,3 @@ biocCache <- function(cache, key, save, update=FALSE) {
 
     path
 }
-
-
--- tests/testthat/test-caching.R
+++ tests/testthat/test-caching.R
@@ -1,7 +1,31 @@
-# This tests the biocCache function.
-# library(testthat); library(zircon); source("test-cache.R")
+# This tests the caching functions.
+# library(testthat); library(zircon); source("test-caching.R")
 
-test_that("caching function works correctly", {
+test_that("simple caching function works correctly", {
+    tmp <- tempfile()
+    dir.create(tmp, recursive=TRUE)
+    cache.fun <- function(key, save) simpleCache(tmp, key, save)
+
+    fname1 <- getFile(example.id, example.url, cache = cache.fun)
+    fname2 <- getFile(example.id, example.url, cache = cache.fun)
+    expect_identical(fname1, fname2)
+    expect_identical(readLines(fname1), LETTERS)
+
+    # Truly respects the cache.
+    txt <- c("Aaron", "is", "a", "little", "lamb")
+    writeLines(txt, con=fname1)
+    fname2 <- getFile(example.id, example.url, cache = cache.fun)
+    expect_identical(fname1, fname2)
+    expect_identical(readLines(fname2), txt) # not changed.
+
+    # Unless we force an update. 
+    cache.fun2 <- function(key, save) simpleCache(tmp, key, save, update=TRUE)
+    fname2 <- getFile(example.id, example.url, cache = cache.fun2)
+    expect_identical(fname1, fname2)
+    expect_identical(readLines(fname2), LETTERS) # changed back.
+})
+
+test_that("BioC caching function works correctly", {
     library(BiocFileCache)
     tmp <- tempfile()
     bfc <- BiocFileCache(tmp, ask=FALSE)
